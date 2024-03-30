188d4abe35eb2dc57ef77f931b9f12f27cbabb9b
Tomáš <awinety@gmail.com>
2024-03-30 09:43:57 +0100

    lepší výběr dat

--- tomovy_erkové_experimenty/datasety.R
+++ tomovy_erkové_experimenty/datasety.R
@@ -1,8 +1,8 @@
 library(here)
 library(dplyr)
 
-orcl_all <- read.csv(here("../data/ORCL_all_orig.csv")) %>% 
-  rename(date = Date, 
+orcl_all <- read.csv(here("../data/ORCL_all_orig.csv")) %>% # načte soubor
+  rename(date = Date,           # přejmenuje sloupce
          open = Open, 
          high = High, 
          low = Low, 
@@ -10,17 +10,14 @@ orcl_all <- read.csv(here("../data/ORCL_all_orig.csv")) %>%
          adjusted_close = Adj.Close, 
          volume = Volume
          ) %>% 
-  mutate(id = row_number()) %>% 
-  select(id, everything())
+  mutate(id = row_number()) %>% # přidá sloupec s ID
+  select(id, everything()) %>%  # přesune sloupec s ID na začátek
+  mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
-total_rows <- nrow(orcl_all)
-split_point <- floor(3/4 * total_rows)
-
-orcl_train <- orcl_all[1:split_point, ]
-orcl_test <- orcl_all[(split_point + 1):total_rows, ]
+# trénovací a testovací data v poměru 3 : 1
+orcl_train <- subset(orcl_all, date < "2017-01-03")
+orcl_test <- subset(orcl_all, date >= "2017-01-03")
 
 write.csv(orcl_all, here("../data/ORCL_all.csv"))
 write.csv(orcl_train, here("../data/ORCL_train.csv"))
 write.csv(orcl_test, here("../data/ORCL_test.csv"))
-
-# orcl <- read.csv(here("../data/ORCL_all.csv"), row.names = 1, header = TRUE)
