fac50a49461374da88cb5bc756b3f12c2b564878
Tomáš <awinety@gmail.com>
2024-04-03 20:12:55 +0200

    R na druhou

--- tomovy_erkové_experimenty/lineární_regrese.R
+++ tomovy_erkové_experimenty/lineární_regrese.R
@@ -2,18 +2,27 @@ library(here)
 library(dplyr)
 library(ggplot2)
 
+# načtení dat --------------------------------------------------------------------------------------
 training_data <- read.csv(here("../data/ORCL_train.csv"), row.names = 1, header = TRUE) %>% 
   mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
 testing_data <- read.csv(here("../data/ORCL_test.csv"), row.names = 1, header = TRUE) %>% 
   mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
+# lin. model a predikování -------------------------------------------------------------------------
 model <- lm(close ~ date, data = training_data)
 
-predicted_training_data <- data.frame(close = predict(model, newdata = training_data), date=training_data$date)
+predicted_training_data <- data.frame(
+  close = predict(model, newdata = training_data), 
+  date=training_data$date
+  )
 
-predicted_testing_data <- data.frame(close = predict(model, newdata = testing_data), date=testing_data$date)
+predicted_testing_data <- data.frame(
+  close = predict(model, newdata = testing_data), 
+  date=testing_data$date
+)
 
+# grafy --------------------------------------------------------------------------------------------
 ggplot(training_data) +
   aes(x = date, y = close) +
   geom_point(size = 0.01) +
@@ -34,3 +43,10 @@ ggplot(rbind(training_data, testing_data)) +
   geom_line(data = rbind(predicted_training_data, predicted_testing_data), color = "purple") +
   theme_linedraw() +
   labs(title = "Lineární model na všech datech", x = "Datum", y = "Hodnota akcie „close“")
+
+# hodnotící metriky --------------------------------------------------------------------------------
+rsq <- function (x, y) cor(x, y) ^ 2
+
+summary(model)$r.squared
+rsq(training_data$close, predicted_training_data$close)
+rsq(testing_data$close, predicted_testing_data$close)
